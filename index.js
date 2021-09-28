d9ffc7d05e805661f18f5ef396e371ac05d9eade
Kadeem140 <Beckford.kadeem@yahoo.com>
2021-09-28 18:25:18 -0400

    Completed Assignment, All tests Passsing

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solutions in this file
+//for ([initialization]; [condition]; [iteration]) {
+//    [loop body]
+//  }
+
+
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+//   }
+
+const array1 = ["Kadeem", "Roshawn", "Mom"];
+
+
+function writeCards(array){
+    let messages = [];
+    for (let i = 0; i < array.length; i++) {
+        messages.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+    }
+    return messages
+}
+
+writeCards(array1)
+
+let i = 10
+function countDown(i){
+    while( i >= 0){
+        console.log(i--)
+    }
+}
+
+countDown(i)
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
